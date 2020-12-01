ffdb2167985e2cf0f92118820334bc19ea392ab5
Michael Fuget <michaelfuget@gmail.com>
2020-12-01 06:02:23 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,15 @@
 def unsafe?(speed)
-
+  if(safe>60 || safe<40)
+    return true 
+  else
+    return false 
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	return ((safe>60 || safe<40) ? true : false)
 end
 	
 
